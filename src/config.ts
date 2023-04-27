4f3fddee2f0057e29432fc6fc85b4c70ba0e8458
Devansh Takkar <devansh@takkar.ooo>
2023-04-27 02:37:58 -0700

    initial commit

---
+++ src/config.ts
@@ -0,0 +1,11 @@
+interface Config{
+    searchQuery: string,
+    message: string
+}
+
+let config: Config = {
+    searchQuery: "vancouver",
+    message: "Hi there, Hope you are doing well"
+}
+
+export default config
---
+++ src/index.ts
@@ -0,0 +1,150 @@
+import puppeteer from "puppeteer-extra";
+import { readFile } from "fs/promises";
+import { Protocol, ElementHandle } from "puppeteer";
+const StealthPlugin = require("puppeteer-extra-plugin-stealth");
+import config from "./config";
+puppeteer.use(StealthPlugin());
+const args = [
+    "--no-sandbox",
+    "--disable-setuid-sandbox",
+    "--disable-infobars",
+    "--window-position=0,0",
+    "--ignore-certifcate-errors",
+    "--ignore-certifcate-errors-spki-list",
+    '--user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3312.0 Safari/537.36"',
+];
+
+const options = {
+    ...args,
+    headless: false,
+};
+async function main() {
+    const browser = await puppeteer.launch(options);
+    let cookies = await readFile("cookies.json", {
+        encoding: "utf8",
+    });
+    let cookieJson: Protocol.Network.CookieParam[] = JSON.parse(cookies);
+    let page = await browser.newPage();
+    await page.setViewport({ width: 1200, height: 720 });
+    await page.setCookie(...cookieJson);
+
+    // open the search page
+    await page.goto(
+        `https://twitter.com/search?q=${config.searchQuery}&src=recent_search_click`
+    );
+    await page.waitForNetworkIdle();
+
+    //create an async iterator object to iterate over the page to find links
+    let asycPageIteratorObj = {
+        [Symbol.asyncIterator]: function () {
+            return {
+                async next() {
+                    let profileLinkFinderScriptResult = await page.evaluate(
+                        () => {
+                            let lastLinkElemPos = Number(
+                                localStorage.getItem("lastLinkElemPos")
+                            );
+                            console.log(lastLinkElemPos);
+                            let newProfileLinks: string[] = [];
+                            let profilePageLinkRegEx = /^\/[0-9A-Za-z]+$/;
+                            let allElementsWithLinks =
+                                document.querySelectorAll("a[role='link']");
+                            allElementsWithLinks.forEach((elem, index) => {
+                                let profileLink = elem.getAttribute("href");
+                                if (profileLink?.match(profilePageLinkRegEx)) {
+                                    if (
+                                        !(
+                                            profileLink == "/home" ||
+                                            profileLink == "/explore" ||
+                                            profileLink == "/notifications" ||
+                                            profileLink == "/messages"
+                                        )
+                                    ) {
+                                        newProfileLinks.push(
+                                            "https://twitter.com" + profileLink
+                                        );
+                                    }
+                                }
+                            });
+                            allElementsWithLinks[
+                                allElementsWithLinks.length - 1
+                            ].scrollIntoView();
+                            return newProfileLinks;
+                        }
+                    );
+                    return {
+                        done: false,
+                        value: profileLinkFinderScriptResult,
+                    };
+                },
+            };
+        },
+    };
+    let visitedUrls: Set<string> = new Set();
+    for await (let profileLinks of asycPageIteratorObj) {
+        console.log(profileLinks);
+        let profileLinksSet: Set<string> = new Set(profileLinks);
+        innerloop: for (let link of profileLinksSet) {
+            // Continue the loop is the url is already visited in the current run
+            if(visitedUrls.has(link)){
+                continue;
+            }
+
+            //Open the profile page in new tab
+            let profilePage = await browser.newPage();
+            await profilePage.goto(link);
+            
+            // Add the link into the visited urls Set to prevent it from opening again and again
+            visitedUrls.add(link);
+            try {
+                let msgBtn = await profilePage.waitForSelector(
+                    "div[data-testid='sendDMFromProfile']",
+                    {
+                        timeout: 5000,
+                    }
+                );
+                await msgBtn!.click();
+                let msgPage = await browser.newPage();
+                await msgPage.goto(profilePage.url());
+                try {
+                    let elem = await msgPage.waitForSelector(
+                        `div[data-testid='DmScrollerContainer'] >>> span ::-p-text(${config.message})`,
+                        {
+                            timeout: 10000,
+                        }
+                    );
+                    msgPage.close().then(() => {
+                        profilePage.close()
+                    })
+                    continue innerloop;
+                } catch (err) {
+                    console.log(err)
+                }
+                let msgBox = await msgPage.waitForSelector(
+                    "div.public-DraftStyleDefault-block",
+                    {
+                        timeout: 5000,
+                    }
+                );
+                await msgBox?.click();
+                await msgBox?.type(config.message);
+                let sendBtn = await msgPage.waitForSelector(
+                    "div[data-testid='dmComposerSendButton']"
+                );
+                await sendBtn?.click();
+                console.log("Message sent");
+                msgPage.waitForNetworkIdle().then(() => {
+                    msgPage.close();
+                    profilePage.close();
+                });
+            } catch (error) {
+                await profilePage.close();
+                console.log(error);
+            }
+        }
+
+        await page.waitForNetworkIdle();
+    }
+}
+
+main();
